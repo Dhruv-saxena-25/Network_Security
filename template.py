1307f0efe14a8964ba49dd08506c972bb6eab066
Dhruv Saxena <saxena25dhruv@gmail.com>
2024-08-17 11:54:51 +0530

    setup

---
+++ template.py
@@ -0,0 +1,28 @@
+import os
+from pathlib import Path
+
+project_name = 'networksecurity'
+
+list_of_files = [
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/pipline/__init__.py",
+    f"{project_name}/utils/__init__.py",
+    f".github/workflows/main.yaml",
+    "app.py",
+    "requirements.txt"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+    else:
+        print(f"file is already present at: {filepath}")
