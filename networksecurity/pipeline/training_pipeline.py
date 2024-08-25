1ff9d54c8efb417ee98394a4ccacdb5a42ca7748
Dhruv Saxena <saxena25dhruv@gmail.com>
2024-08-25 12:15:27 +0530

    model evaluation and pusher

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -101,16 +101,23 @@ class TrainingPipeline:
             raise NetworkSecurityException(e, sys)
         
     
-    def start_model_evaluation(self):
+    def start_model_evaluation(self, data_validation_artifact:DataValidationArtifact,
+                                 model_trainer_artifact:ModelTrainerArtifact):
         try:
-            pass
+            model_eval_config: ModelEvaluationConfig = ModelEvaluationConfig(training_pipeline_config= self.training_pipeline_config)
+            model_eval = ModelEvaluation(model_eval_config, data_validation_artifact, model_trainer_artifact)
+            model_eval_artifact = model_eval.initiate_model_evaluation()
+            return model_eval_artifact
 
         except Exception as e:
             raise NetworkSecurityException(e, sys)
     
-    def start_model_pusher(self):
+    def start_model_pusher(self, model_eval_artifact:ModelEvaluationArtifact):
         try:
-            pass
+            model_pusher_config = ModelPusherConfig(training_pipeline_config=self.training_pipeline_config)
+            model_pusher = ModelPusher(model_pusher_config, model_eval_artifact)
+            model_pusher_artifact = model_pusher.initiate_model_pusher()
+            return model_pusher_artifact
 
         except Exception as e:
             raise NetworkSecurityException(e, sys)
@@ -124,7 +131,12 @@ class TrainingPipeline:
             print(data_validation_artifact)
             data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
             model_trainer_artifact: ModelTrainerArtifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
-
+            model_eval_artifact = self.start_model_evaluation(data_validation_artifact, model_trainer_artifact)
+            if not model_eval_artifact.is_model_accepted:
+                raise Exception("Trained model is not better than the best model")
+            model_pusher_artifact = self.start_model_pusher(model_eval_artifact)
+        
+            
         except Exception as e:
             raise NetworkSecurityException(e, sys)
         
