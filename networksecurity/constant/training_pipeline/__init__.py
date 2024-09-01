7333f13c4a62887239e55eabe8e94f5d21445ed0
Dhruv Saxena <saxena25dhruv@gmail.com>
2024-09-01 11:55:24 +0530

    training constant

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -87,7 +87,7 @@ MODEL_PUSHER_SAVED_MODEL_DIR = SAVED_MODEL_DIR
 
 
 TRAINING_BUCKET_NAME = "mynetworksecurities"
-PREDICTION_BUCKET_NAME = "Network-dataSource"
+PREDICTION_BUCKET_NAME = "network-datasource-prediction"
 PREDICTION_DIR="prediction"
 
 
